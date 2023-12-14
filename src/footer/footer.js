a73a41ec29e89dbc045cfaa631748d77a5b4a6af
isidkoma <143963038+isidkoma@users.noreply.github.com>
2023-12-14 01:00:37 -0500

    Add files via upload

---
+++ src/footer/footer.js
@@ -0,0 +1,22 @@
+
+/* eslint-disable react/no-unknown-property */
+import React from 'react';
+
+
+function footer() {
+  return (
+    <div>
+       <footer className="bottom">
+        <div className="center">
+        <p>
+  All rights reserved &copy; <span style={{ color: 'red' }}>Siddhartha Komakula</span> 2023
+</p>
+
+        </div>
+       
+    </footer>
+    </div>
+  );
+}
+
+export default footer;
